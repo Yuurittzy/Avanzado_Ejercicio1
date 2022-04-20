1ac0011620fe3b84bc4e245e74266556adc906d1
Yu <yu@Yus-MacBook-Pro.local>
2022-04-19 22:58:49 -0500

    ejercicio1

---
+++ app/src/main/java/com/example/beauty_and_beast/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.beauty_and_beast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
